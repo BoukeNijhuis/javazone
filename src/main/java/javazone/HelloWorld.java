61e522737260832e00de5766a16e64e37dc94f87
Bouke Nijhuis <bouke.nijhuis@gmail.com>
2022-08-06 20:18:23 +0200

    changed code some stepping can be explained better

--- src/main/java/javazone/HelloWorld.java
+++ src/main/java/javazone/HelloWorld.java
@@ -5,7 +5,8 @@ public class HelloWorld {
     private static final String DEFAULT_VALUE = "World";
 
     public static void main(String[] args) {
-        System.out.println(createMessage(args));
+        String message = createMessage(args);
+        System.out.println(message);
     }
 
     private static String createMessage(String[] args) {
@@ -15,6 +16,6 @@ public class HelloWorld {
             name = args[0];
         }
 
-        return String.format("Hello %s!", name);
+        return String.format("Hallo %s!", name);
     }
 }
